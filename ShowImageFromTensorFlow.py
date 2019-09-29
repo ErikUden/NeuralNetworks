dde34afa6dfb7046d6922bde319aadd471db586d
Not Erik Uden <52011431+NotErikUde@users.noreply.github.com>
2019-09-28 22:35:18 -0700

    Uploaded first program
    With the "ShowImage[...].py" program you can display any image installed with the keras Neural Network training data package. The classes are well defined and everything that is needed is imported.

---
+++ ShowImageFromTensorFlow.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+
+
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+
+plt.imshow(train_images[7], cmap=plt.cm.binary)
+plt.show()
--- firsttensorflow.py
+++ firsttensorflow.py
@@ -11,4 +11,10 @@ data = keras.datasets.fashion_mnist
 
 
 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+
+
+plt.imshow(train_images[7], cmap=plt.cm.binary)
+plt.show()
