4ac9f16a7580ff9a9d56f5e553a3336da06377dc
Not Erik Uden <52011431+NotErikUde@users.noreply.github.com>
2019-10-04 19:48:23 -0700

    Syntax

--- CurrentTensorFlow.py
+++ CurrentTensorFlow.py
@@ -1,9 +1,4 @@
-import tensorflow as tf ; from tensorflow import keras ; import numpy as np ; import matplotlib.pyplot as plt
-data = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = data.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-train_images=train_images/255.0
-test_images=test_images/255.0
+import tensorflow as tf ; from tensorflow import keras ; import numpy as np ; import matplotlib.pyplot as plt ; data = keras.datasets.fashion_mnist ; (train_images, train_labels), (test_images, test_labels) = data.load_data() ; class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'] ; train_images=train_images/255.0 ; test_images=test_images/255.0
 model = keras.Sequential([
 	keras.layers.Flatten(input_shape=(28,28)),
 	keras.layers.Dense(128, activation="relu"),
